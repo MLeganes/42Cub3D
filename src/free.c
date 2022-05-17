821c40f6566269c608a7f8150d80b5a7d3138d00
Angel Morcillo leganes <amorcill@1-A-2.42heilbronn.de>
2022-05-17 12:37:37 +0200

    Restore free.c

--- src/free.c
+++ src/free.c
@@ -1,40 +1,4 @@
-HA:
-    - Vokabelwiederholung: 
-        AB. S. 74-75
-    - AB. S. 76, 77, 78
-    - Zusatzblatt Passiv (Moodle)
-    - Vokabelheft kaufen 
-    
-
-...wenn man vom Teufel spricht
-
-Idiome vom 16.05.:
-    - den Bund fürs Leben schließen
-    - auf gut Deutsch gesagt
-    - auf de.m falschen Dampfer sein
-    - im Eimer sein
-    (müde / erledigt / erschöpft)
-    - wie ein Elefant im Porzellanladen
-    (ungeschickt / tollpatschig) 
-    mit: sich bewegen / sich verhalten / aufteten / sein )HA:
-    - Vokabelwiederholung: 
-        AB. S. 74-75
-    - AB. S. 76, 77, 78
-    - Zusatzblatt Passiv (Moodle)
-    - Vokabelheft kaufen 
-    
-
-...wenn man vom Teufel spricht
-
-Idiome vom 16.05.:
-    - den Bund fürs Leben schließen
-    - auf gut Deutsch gesagt
-    - auf de.m falschen Dampfer sein
-    - im Eimer sein
-    (müde / erledigt / erschöpft)
-    - wie ein Elefant im Porzellanladen
-    (ungeschickt / tollpatschig) 
-    mit: sich bewegen / sich verhalten / aufteten / sein )#include "cub3d.h"
+#include "cub3d.h"
 
 void free_cub3d(t_cub3d *cub)
 {
